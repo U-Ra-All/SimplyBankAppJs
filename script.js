47fbae7502b70b16bda1452aef0873f7c0a3fbfa
YouRa <allakhverdovyuriy@gmail.com>
2021-10-06 08:30:32 +0300

    Implement transaction sorting

--- script.js
+++ script.js
@@ -65,10 +65,14 @@ const inputLoanAmount = document.querySelector('.form__input--loan-amount');
 const inputCloseNickname = document.querySelector('.form__input--user');
 const inputClosePin = document.querySelector('.form__input--pin');
 
-const displayTransactions = function (transactions) {
+const displayTransactions = function (transactions, sort = false) {
   containerTransactions.innerHTML = '';
 
-  transactions.forEach(function (trans, index) {
+  const transacs = sort
+    ? transactions.slice().sort((x, y) => x - y)
+    : transactions;
+
+  transacs.forEach(function (trans, index) {
     const transType = trans > 0 ? 'deposit' : 'withdrawal';
 
     const transactionRow = `
@@ -228,4 +232,12 @@ btnLoan.addEventListener('click', function (e) {
     updateUi(currentAccount);
   }
   inputLoanAmount.value = '';
+});
+
+let transactionsSorted = false;
+
+btnSort.addEventListener('click', function (e) {
+  e.preventDefault();
+  displayTransactions(currentAccount.transactions, !transactionsSorted);
+  transactionsSorted = !transactionsSorted;
 });
