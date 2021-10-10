b81e4c659a446c034885e44ae169c04bcdf8c949
YouRa <allakhverdovyuriy@gmail.com>
2021-10-10 16:26:43 +0300

    03-Вносим Правки В Приложение

--- script.js
+++ script.js
@@ -4,7 +4,7 @@
 
 const account1 = {
   userName: 'Cecil Ireland',
-  transactions: [500, 250, -300, 5000, -850, -110, -170, 1100],
+  transactions: [500.32, 250, -300.92, 5000, -850, -110.18, -170, 1100],
   interest: 1.5,
   pin: 1111,
 };
@@ -80,7 +80,7 @@ const displayTransactions = function (transactions, sort = false) {
       <div class="transactions__type transactions__type--${transType}">
         ${index + 1} ${transType}
       </div>
-      <div class="transactions__value">${trans}</div>
+      <div class="transactions__value">${trans.toFixed(2)}</div>
     </div>
     `;
     containerTransactions.insertAdjacentHTML('afterbegin', transactionRow);
@@ -114,19 +114,19 @@ createNicknames(accounts);
 const displayBalance = function (account) {
   const balance = account.transactions.reduce((acc, trans) => acc + trans, 0);
   account.balance = balance;
-  labelBalance.textContent = `${balance}$`;
+  labelBalance.textContent = `${balance.toFixed(2)}$`;
 };
 
 const displayTotal = function (account) {
   const depositesTotal = account.transactions
     .filter(trans => trans > 0)
     .reduce((acc, trans) => acc + trans, 0);
-  labelSumIn.textContent = `${depositesTotal}$`;
+  labelSumIn.textContent = `${depositesTotal.toFixed(2)}$`;
 
   const withdrawalsTotal = account.transactions
     .filter(trans => trans < 0)
     .reduce((acc, trans) => acc + trans, 0);
-  labelSumOut.textContent = `${withdrawalsTotal}$`;
+  labelSumOut.textContent = `${withdrawalsTotal.toFixed(2)}$`;
 
   const interestTotal = account.transactions
     .filter(trans => trans > 0)
@@ -136,7 +136,7 @@ const displayTotal = function (account) {
       return interest >= 5;
     })
     .reduce((acc, interest) => acc + interest, 0);
-  labelSumInterest.textContent = `${interestTotal}$`;
+  labelSumInterest.textContent = `${interestTotal.toFixed(2)}$`;
 };
 
 const updateUi = function (account) {
@@ -161,7 +161,7 @@ btnLogin.addEventListener('click', function (e) {
   );
   console.log(currentAccount);
 
-  if (currentAccount?.pin === Number(inputLoginPin.value)) {
+  if (currentAccount?.pin === +inputLoginPin.value) {
     // Display UI and welcome message
     containerApp.style.opacity = 100;
 
@@ -180,7 +180,7 @@ btnLogin.addEventListener('click', function (e) {
 
 btnTransfer.addEventListener('click', function (e) {
   e.preventDefault();
-  const transferAmount = Number(inputTransferAmount.value);
+  const transferAmount = +inputTransferAmount.value;
   const recipientNickname = inputTransferTo.value;
   const recipientAccount = accounts.find(
     account => account.nickname === recipientNickname
@@ -204,7 +204,7 @@ btnClose.addEventListener('click', function (e) {
   e.preventDefault();
   if (
     inputCloseNickname.value === currentAccount.nickname &&
-    Number(inputClosePin.value) === currentAccount.pin
+    +inputClosePin.value === currentAccount.pin
   ) {
     const currentAccountIndex = accounts.findIndex(
       account => account.nickname === currentAccount.nickname
@@ -222,7 +222,7 @@ btnClose.addEventListener('click', function (e) {
 
 btnLoan.addEventListener('click', function (e) {
   e.preventDefault();
-  const loanAmount = Number(inputLoanAmount.value);
+  const loanAmount = Math.floor(inputLoanAmount.value);
 
   if (
     loanAmount > 0 &&
@@ -251,7 +251,7 @@ btnSort.addEventListener('click', function (e) {
 //   // const transactionsUiArray = Array.from(transactionsUi);
 //   // console.log(transactionsUiArray.map(elem => Number(elem.textContent)));
 //   const transactionsUiArray = Array.from(transactionsUi, elem =>
-//     Number(elem.textContent)
+//     +(elem.textContent)
 //   );
 //   console.log(transactionsUiArray);
 // });
